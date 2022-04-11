57cc8c89028f678806a3429a03cf3eddb90b4433
Martin Hinga <martin.hinga@outlook.com>
2022-04-11 19:00:39 +0300

    modify url by adding https

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'martin-hin.github.io',
+  url: 'https://martin-hin.github.io',
   baseUrl: '/studyNotes/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
